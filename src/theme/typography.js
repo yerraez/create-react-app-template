07c267a19de6b44195792645959eeec2dfdae7a0
Jona <jona@Jonathans-MacBook-Pro.local>
2020-11-07 17:26:20 -0500

    Edit font family

--- src/theme/typography.js
+++ src/theme/typography.js
@@ -1,80 +1,80 @@
 export default {
-  fontFamily: ["AIGFutura-Medium", "AIGFutura-Book", "AIGFutura-Bold"].join(","),
+  fontFamily: ["Roboto"].join(","),
   button: {
     fontWeight: 500,
-    fontFamily: "AIGFutura-Medium",
+    fontFamily: "Roboto",
   },
   h1: {
     fontWeight: 500,
-    fontFamily: "AIGFutura-Medium",
+    fontFamily: "Roboto",
     fontSize: 35,
     letterSpacing: "-0.24px",
   },
   h2: {
     fontWeight: 500,
-    fontFamily: "AIGFutura-Medium",
+    fontFamily: "Roboto",
     fontSize: 29,
     letterSpacing: "-0.24px",
   },
   h3: {
     fontWeight: 500,
-    fontFamily: "AIGFutura-Medium",
+    fontFamily: "Roboto",
     fontSize: 24,
     letterSpacing: "-0.06px",
   },
   h4: {
     fontWeight: 500,
-    fontFamily: "AIGFutura-Medium",
+    fontFamily: "Roboto",
     fontSize: 20,
     letterSpacing: "-0.06px",
   },
   h5: {
     fontWeight: 500,
-    fontFamily: "AIGFutura-Medium",
+    fontFamily: "Roboto",
     fontSize: 16,
     letterSpacing: "-0.05px",
   },
   h6: {
     fontWeight: 500,
-    fontFamily: "AIGFutura-Medium",
+    fontFamily: "Roboto",
     fontSize: 14,
     letterSpacing: "-0.05px",
   },
   outlined: {
     fontWeight: 500,
-    fontFamily: "AIGFutura-Medium",
+    fontFamily: "Roboto",
     fontSize: 14,
     letterSpacing: "-0.05px",  
   },
   overline: {
     fontWeight: 500,
-    fontFamily: "AIGFutura-Medium",
+    fontFamily: "Roboto",
   },
 
   body1: {
     fontWeight: 500,
-    fontFamily: "AIGFutura-Medium",
+    fontFamily: "Roboto",
   },
 
   body2: {
     fontWeight: 500,
-    fontFamily: "AIGFutura-Medium",
+    fontFamily: "Roboto",
   },
 
   subtitle1: {
     fontWeight: 500,
-    fontFamily: "AIGFutura-Medium",
+    fontFamily: "Roboto",
   },
 
   subtitle2: {
     fontWeight: 500,
-    fontFamily: "AIGFutura-Medium",
+    fontFamily: "Roboto",
   },
 
 
   caption: {
     fontWeight: 500,
-    fontFamily: "AIGFutura-Medium",
+    fontFamily: "Roboto",
   },
 
 
